f5e18e00fcf0d31829d99c1eac145e4e403ab9ed
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-06-12 18:28:52 +0200

    Ajout de la methode de classe findByPeopleId dans MovieCollection

--- src/Entity/Collection/MovieCollection.php
+++ src/Entity/Collection/MovieCollection.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Entity\Collection;
@@ -16,12 +17,32 @@ class MovieCollection
     {
         $stmt = MyPdo::getInstance()->prepare(
             <<<SQL
-SELECT posterId, id, originalLanguage, originalTitle, 
-       overview, releaseDate, runtime, tagline, title
-FROM movie
-ORDER BY title;
-SQL
+            SELECT posterId, id, originalLanguage, originalTitle, 
+                   overview, releaseDate, runtime, tagline, title
+            FROM movie
+            ORDER BY title;
+            SQL
         );
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Movie::class);
+    }
+
+    public static function findByPeopleId($peopleId)
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
+            SELECT DISTINCT posterId, id, originalLanguage, originalTitle, 
+                   overview, releaseDate, runtime, tagline, title
+            FROM movie m
+                JOIN cast c ON m.id = c.movieId
+            WHERE peopleId = :peopleId
+            ORDER BY title;
+            SQL
+        );
+
+        $stmt->bindValue(':peopleId', "$peopleId");
+
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_CLASS, Movie::class);
     }
