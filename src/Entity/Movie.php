50cb468d0de6e07312a27a4904a4fe5a1908464a
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-06-12 10:25:54 +0200

    Création de la classe movie

---
+++ src/Entity/Movie.php
@@ -0,0 +1,181 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class Movie
+{
+    private int $posterId;
+    private int $id;
+    private string $originalLanguage;
+    private string $orignalTitle;
+    private string $overview;
+    private string $releaseDate;
+    private int $runtime;
+    private string $tagline;
+    private string $title;
+
+    /**
+     * @return int
+     */
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }
+
+    /**
+     * @param int $posterId
+     * @return Movie
+     */
+    public function setPosterId(int $posterId): Movie
+    {
+        $this->posterId = $posterId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Movie
+     */
+    public function setId(int $id): Movie
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOriginalLanguage(): string
+    {
+        return $this->originalLanguage;
+    }
+
+    /**
+     * @param string $originalLanguage
+     * @return Movie
+     */
+    public function setOriginalLanguage(string $originalLanguage): Movie
+    {
+        $this->originalLanguage = $originalLanguage;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrignalTitle(): string
+    {
+        return $this->orignalTitle;
+    }
+
+    /**
+     * @param string $orignalTitle
+     * @return Movie
+     */
+    public function setOrignalTitle(string $orignalTitle): Movie
+    {
+        $this->orignalTitle = $orignalTitle;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    /**
+     * @param string $overview
+     * @return Movie
+     */
+    public function setOverview(string $overview): Movie
+    {
+        $this->overview = $overview;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReleaseDate(): string
+    {
+        return $this->releaseDate;
+    }
+
+    /**
+     * @param string $releaseDate
+     * @return Movie
+     */
+    public function setReleaseDate(string $releaseDate): Movie
+    {
+        $this->releaseDate = $releaseDate;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRuntime(): int
+    {
+        return $this->runtime;
+    }
+
+    /**
+     * @param int $runtime
+     * @return Movie
+     */
+    public function setRuntime(int $runtime): Movie
+    {
+        $this->runtime = $runtime;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTagline(): string
+    {
+        return $this->tagline;
+    }
+
+    /**
+     * @param string $tagline
+     * @return Movie
+     */
+    public function setTagline(string $tagline): Movie
+    {
+        $this->tagline = $tagline;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Movie
+     */
+    public function setTitle(string $title): Movie
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+
+}
