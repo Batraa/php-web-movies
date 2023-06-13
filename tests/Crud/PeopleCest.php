3b2cd5b0dd8ca6c8952b703330e3301ded0609af
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-06-13 14:31:44 +0200

    ajout de certains tests de la classe People

---
+++ tests/Crud/PeopleCest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tests\Crud;
+use Entity\Exception\EntityNotFoundException;
+use Entity\People;
+use Tests\CrudTester;
+class PeopleCest
+{
+    public function findById(CrudTester $I)
+    {
+        $people = People::findById(1145);
+        $I->assertSame(1145, $people->getId());
+        $I->assertSame('Zbigniew Zamachowski', $people->getName());
+    }
+
+    public function findByIdThrowsExceptionIfArtistDoesNotExist(CrudTester $I)
+    {
+        $I->expectThrowable(EntityNotFoundException::class, function () {
+            People::findById(PHP_INT_MAX);
+        });
+    }
+}
