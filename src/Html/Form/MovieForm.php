d0afe4002c1efd08bb2bd997793b0c78aef23491
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-06-14 16:33:07 +0200

    Ajout de la methode d'instance getHtmlForm

--- src/Html/Form/MovieForm.php
+++ src/Html/Form/MovieForm.php
@@ -29,4 +29,20 @@ class MovieForm
     {
         return $this->movie;
     }
+    public function getHtmlForm(string $action): string
+    {
+        return <<<HTML
+                    <form name="form" method="POST" action="$action">
+                        <input name="id" type="hidden" value="{$this->getMovie()?->getId()}">
+                        <label>Titre<input required="required" name="Title" type="text" value="{$this->escapeString($this->getMovie()?->getTitle())}"></label> 
+                        <label>Titre original<input required="required" name="originalTitle" type="text" value="{$this->escapeString($this->getMovie()?->getOriginalTitle())}"></label> 
+                        <label>Langue originale<input required="required" name="originalLanguage" type="text" value="{$this->escapeString($this->getMovie()?->getOriginalLanguage())}"></label>
+                        <label>Description<input required="required" name="overview" type="text" value="{$this->escapeString($this->getMovie()?->getOverview())}"></label>
+                        <label>Date de sortie<input required="required" name="releaseDate" type="text" value="{$this->escapeString($this->getMovie()?->getReleaseDate())}"></label>
+                        <label>Durée<input required="required" name="runtime" type="text" value="{$this->escapeString($this->getMovie()?->getRuntime())}"></label>
+                        <label>Slogan<input required="required" name="tagline" type="text" value="{$this->escapeString($this->getMovie()?->getTagline())}"></label>
+                        <button type="submit">Enregistrer</button>	
+                    </form>                 
+            HTML;
+    }
 }
