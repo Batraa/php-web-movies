970fdffa7a695e0ea5b5ab1699f4c478b2b2e42f
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-06-14 12:37:40 +0200

    ajout des tests de la classe Image.php

--- tests/Crud/ImageCest.php
+++ tests/Crud/ImageCest.php
@@ -9,18 +9,18 @@ use Tests\CrudTester;
 
 class ImageCest
 {
-    /*public function findImgageById(CrudTester $I)
+    public function findImgageById(CrudTester $I)
     {
         $cover = Image::findById(16731);
         $I->assertSame(16731, $cover->getId());
-        $I->assertSame(file_get_contents(codecept_data_dir() . '/img/'), $cover->getJpeg());
+        $I->assertSame(file_get_contents(codecept_data_dir() . '/img/imgPeople.jpg'), $cover->getJpeg());
     }
     public function findImageByIdThrowsExceptionIfImageDoesNotExist(CrudTester $I)
     {
         $I->expectThrowable(EntityNotFoundException::class, function () {
             Image::findById(PHP_INT_MAX);
         });
-    }*/
+    }
 }
 
 
