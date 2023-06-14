c1fd5e6460c9f6721b6fea8d480f8edcbeb6836f
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-06-14 10:46:01 +0200

    Modification de la methode de classe create

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -20,16 +20,7 @@ class Movie
     private string $tagline;
     private string $title;
 
-    private function __construct(string $originalLanguage, string $originalTitle, string $overview, string $releaseDate, int $runtime, string $tagline, string $title)
-    {
-        $this->originalLanguage = $originalLanguage;
-        $this->originalTitle = $originalTitle;
-        $this->overview = $overview;
-        $this->releaseDate = $releaseDate;
-        $this->runtime = $runtime;
-        $this->tagline = $tagline;
-        $this->title = $title;
-    }
+
 
     /**
      * @return int
@@ -265,7 +256,16 @@ SQL
 
     public static function create(string $originalLanguage, string $originalTitle, string $overview, string $releaseDate, int $runtime, string $tagline, string $title)
     {
-        return new Movie($originalLanguage, $originalTitle, $overview, $releaseDate, $runtime, $tagline, $title);
+        $movie = new Movie();
+        $movie->setOriginalLanguage($originalLanguage);
+        $movie->setOriginalTitle($originalTitle);
+        $movie->setOverview($overview);
+        $movie->setReleaseDate($releaseDate);
+        $movie->setRuntime($runtime);
+        $movie->setTagline($tagline);
+        $movie->setTitle($title);
+
+        return $movie;
     }
 
     public function insert(): Movie
