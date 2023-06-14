938f1bd179512bae847159427c3a0be716b2eff1
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-06-14 18:23:40 +0200

    Ajout d'un bouton pour ajouter un film

--- public/index.php
+++ public/index.php
@@ -9,6 +9,11 @@ $webpage = new AppWebPage("Liste des films");
 
 $movies = MovieCollection::findAll();
 
+$webpage->appendContent("
+                        <form action='/admin/movie-form.php' method='POST'>
+                            <input type='submit' value='Ajouter'>
+                        </form>");
+
 foreach ($movies as $movie) {
     $webpage->appendContent("<a href='film.php?movieId={$movie->getId()}'>
         <section class='movie__content'><img class='movie__image' src='imgMovie.php?imageId={$movie->getPosterId()}'>
