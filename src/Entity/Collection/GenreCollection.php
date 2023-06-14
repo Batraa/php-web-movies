c5c7db38ddd286f0ec0a5f90df3546b40a9cf107
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-06-14 23:05:18 +0200

    Ajout de la methode findAll

--- src/Entity/Collection/GenreCollection.php
+++ src/Entity/Collection/GenreCollection.php
@@ -3,7 +3,21 @@ declare(strict_types=1);
 
 namespace Entity\Collection;
 
-class GenderCollection
-{
+use Database\MyPdo;
+use Entity\Genre;
 
+class GenreCollection
+{
+    public static function findAll(): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
+SELECT id, name
+FROM genre
+ORDER BY name
+SQL
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, Genre::class);
+    }
 }
