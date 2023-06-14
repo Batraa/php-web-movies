18cc73206a508411572b975a32513ed2ddb0704c
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-06-14 17:33:36 +0200

    Ajout de la methode setEntityFromQueryString

--- src/Html/Form/MovieForm.php
+++ src/Html/Form/MovieForm.php
@@ -34,7 +34,7 @@ class MovieForm
         return <<<HTML
                     <form name="form" method="POST" action="$action">
                         <input name="id" type="hidden" value="{$this->getMovie()?->getId()}">
-                        <label>Titre<input required="required" name="Title" type="text" value="{$this->escapeString($this->getMovie()?->getTitle())}"></label> 
+                        <label>Titre<input required="required" name="title" type="text" value="{$this->escapeString($this->getMovie()?->getTitle())}"></label> 
                         <label>Titre original<input required="required" name="originalTitle" type="text" value="{$this->escapeString($this->getMovie()?->getOriginalTitle())}"></label> 
                         <label>Langue originale<input required="required" name="originalLanguage" type="text" value="{$this->escapeString($this->getMovie()?->getOriginalLanguage())}"></label>
                         <label>Description<input required="required" name="overview" type="text" value="{$this->escapeString($this->getMovie()?->getOverview())}"></label>
@@ -44,5 +44,66 @@ class MovieForm
                         <button type="submit">Enregistrer</button>	
                     </form>                 
             HTML;
+    }
+
+    /**
+     * @throws ParameterException
+     */
+    public function setEntityFromQueryString(): void
+    {
+
+
+        if (empty($_POST['id']) || !ctype_digit($_POST['id'])) {
+            $id = null;
+        } else {
+            $id = ((int)$_POST['id']);
+        }
+
+        if (empty($_POST['title'])) {
+            throw new ParameterException("Le titre du film n'est pas présent");
+        } else {
+            $title = $this->stripTagsAndTrim($_POST['title']);
+        }
+
+        if (empty($_POST['originalTitle'])) {
+            throw new ParameterException("Le titre original du film n'est pas présent");
+        } else {
+            $originalTitle = $this->stripTagsAndTrim($_POST['originalTitle']);
+        }
+
+        if (empty($_POST['originalLanguage'])) {
+            throw new ParameterException("La langue originale du film n'est pas présente");
+        } else {
+            $originalLanguage = $this->stripTagsAndTrim($_POST['originalLanguage']);
+        }
+
+        if (empty($_POST['overview'])) {
+            throw new ParameterException("La description du film n'est pas présente");
+        } else {
+            $overview = $this->stripTagsAndTrim($_POST['overview']);
+        }
+
+        if (empty($_POST['releaseDate'])) {
+            throw new ParameterException("la date de sortie du film n'est pas présente");
+        } else {
+            $releaseDate = $this->stripTagsAndTrim($_POST['releaseDate']);
+        }
+
+        if (empty($_POST['runtime']) || ctype_digit($_POST['runtime'])) {
+            throw new ParameterException("La durée du film n'est pas présente");
+        } else {
+            $runtime = $this->stripTagsAndTrim($_POST['runtime']);
+        }
+
+        if (empty($_POST['tagline'])) {
+            throw new ParameterException("Le slogan du film n'est pas présente");
+        } else {
+            $tagline = $this->stripTagsAndTrim($_POST['tagline']);
+        }
+
+        $movie = Movie::create($originalLanguage, $originalTitle, $overview, $releaseDate, intval($runtime), $tagline, $title, $id);
+
+        $this->movie = $movie;
+
     }
 }
