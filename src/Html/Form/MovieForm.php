97320ae271a1ecd9d9c866e2b028d918197dd3cc
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-06-14 22:45:53 +0200

    Correction du fichier, formulaire fonctionnel

--- src/Html/Form/MovieForm.php
+++ src/Html/Form/MovieForm.php
@@ -45,7 +45,7 @@ class MovieForm
                         <p>
                         <label>Date de sortie<input required="required" name="releaseDate" type="text" value="{$this->escapeString($this->getMovie()?->getReleaseDate())}"></label>
                         <p>
-                        <label>Durée<input required="required" name="runtime" type="text" value="{$this->escapeString($this->getMovie()?->getRuntime())}"></label>
+                        <label>Durée<input required="required" name="runtime" type="text" value="{$this->escapeString(strval($this->getMovie()?->getRuntime()))}"></label>
                         <p>
                         <label>Slogan<input required="required" name="tagline" type="text" value="{$this->escapeString($this->getMovie()?->getTagline())}"></label>
                         <p>
@@ -61,49 +61,49 @@ class MovieForm
     {
 
 
-        if (empty($_POST['id']) || !ctype_digit($_POST['id'])) {
+        if (!isset($_POST['id']) || !ctype_digit($_POST['id'])) {
             $id = null;
         } else {
             $id = ((int)$_POST['id']);
         }
 
-        if (empty($_POST['title'])) {
+        if (!isset($_POST['title'])) {
             throw new ParameterException("Le titre du film n'est pas présent");
         } else {
             $title = $this->stripTagsAndTrim($_POST['title']);
         }
 
-        if (empty($_POST['originalTitle'])) {
+        if (!isset($_POST['originalTitle'])) {
             throw new ParameterException("Le titre original du film n'est pas présent");
         } else {
             $originalTitle = $this->stripTagsAndTrim($_POST['originalTitle']);
         }
 
-        if (empty($_POST['originalLanguage'])) {
+        if (!isset($_POST['originalLanguage'])) {
             throw new ParameterException("La langue originale du film n'est pas présente");
         } else {
             $originalLanguage = $this->stripTagsAndTrim($_POST['originalLanguage']);
         }
 
-        if (empty($_POST['overview'])) {
+        if (!isset($_POST['overview'])) {
             throw new ParameterException("La description du film n'est pas présente");
         } else {
             $overview = $this->stripTagsAndTrim($_POST['overview']);
         }
 
-        if (empty($_POST['releaseDate'])) {
+        if (!isset($_POST['releaseDate'])) {
             throw new ParameterException("la date de sortie du film n'est pas présente");
         } else {
             $releaseDate = $this->stripTagsAndTrim($_POST['releaseDate']);
         }
 
-        if (empty($_POST['runtime']) || ctype_digit($_POST['runtime'])) {
+        if (!isset($_POST['runtime']) || !ctype_digit($_POST['runtime'])) {
             throw new ParameterException("La durée du film n'est pas présente");
         } else {
             $runtime = $this->stripTagsAndTrim($_POST['runtime']);
         }
 
-        if (empty($_POST['tagline'])) {
+        if (!isset($_POST['tagline'])) {
             throw new ParameterException("Le slogan du film n'est pas présente");
         } else {
             $tagline = $this->stripTagsAndTrim($_POST['tagline']);
